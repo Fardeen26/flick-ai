1345b19b64c17f309951dafac32ffc8792b5750a
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2025-01-12 22:07:57 +0530

    feat: appbar structure created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Appbar from "@/components/Appbar";
 import Hero from "@/components/Hero";
 import Main from "@/components/Main";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-8 justify-center items-center h-screen">
-      <Appbar />
+    <div className="flex flex-col gap-8 justify-center items-center mt-40">
       <Hero />
       <Main />
     </div>
--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -3,16 +3,18 @@ import Link from "next/link";
 
 export default function Appbar() {
     return (
-        <header>
-            <h1>
-                <Link href="/">
-                    <Image src="/next.svg" alt="logo" width={100} height={100} />
-                </Link>
-            </h1>
-            <nav>
-                <ul>
+        <header className="bg-white text-black w-[50vw] p-4 mt-6 rounded-lg">
+            <nav className="flex items-center justify-between">
+                <h1>
+                    <Link href="/">
+                        <Image src="/next.svg" alt="logo" width={100} height={100} />
+                    </Link>
+                </h1>
+
+                <ul className="flex gap-4">
                     <li><Link href="/">Home</Link></li>
                     <li><Link href="/about">About</Link></li>
+                    <li><Link href="/about">Developer</Link></li>
                 </ul>
             </nav>
         </header>
