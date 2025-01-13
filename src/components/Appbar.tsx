7d9a904c5d0034b33f4fda7e43a044ba5b239713
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2025-01-13 22:37:57 +0530

    fix: updated appbar

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -1,20 +1,29 @@
 import Image from "next/image";
 import Link from "next/link";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+import { GiWireframeGlobe } from "react-icons/gi";
 
 export default function Appbar() {
     return (
-        <header className="bg-white text-black w-[50vw] p-4 mt-6 rounded-lg">
+        <header className="w-full p-2 mt-4 rounded-lg px-12">
             <nav className="flex items-center justify-between">
                 <h1>
                     <Link href="/">
-                        <Image src="/next.svg" alt="logo" width={100} height={100} />
+                        <Image src="/vercel.svg" alt="logo" width={15} height={15} />
                     </Link>
                 </h1>
 
                 <ul className="flex gap-4">
-                    <li><Link href="/">Home</Link></li>
-                    <li><Link href="/about">About</Link></li>
-                    <li><Link href="/about">Developer</Link></li>
+                    <li><Link href="/">
+                        <FaSquareXTwitter className="w-4 h-4" />
+                    </Link></li>
+                    <li><Link href="/about">
+                        <GiWireframeGlobe className="w-4 h-4" />
+                    </Link></li>
+                    <li><Link href="/about">
+                        <FaGithub className="w-4 h-4" />
+                    </Link></li>
                 </ul>
             </nav>
         </header>
