dcfb0608fba063887b3f1d377c19c6e0998ef53c
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2025-01-23 14:39:15 +0530

    feat: storing user in database

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 const handler = NextAuth({
     providers: [
@@ -9,6 +11,31 @@ const handler = NextAuth({
         })
     ],
     secret: process.env.NEXTAUTH_SECRET,
+    callbacks: {
+        async signIn({ user }) {
+            try {
+                const existingUser = await prisma.user.findUnique({
+                    where: { email: user.email! },
+                });
+
+                if (!existingUser) {
+                    await prisma.user.create({
+                        data: {
+                            email: user.email!,
+                            name: user.name || null,
+                            profileImage: user.image || null,
+                        },
+                    });
+                }
+                return true;
+            } catch (error) {
+                console.error("Error during sign-in:", error);
+                return false;
+            } finally {
+                await prisma.$disconnect();
+            }
+        },
+    },
     session: {
         strategy: "jwt",
     },
