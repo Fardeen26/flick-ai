387f9dc5bcbdc68cd5cf3485741a1ad9a427bbf9
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2025-01-23 13:39:25 +0530

    feat: added credit based limit system

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,25 +1,15 @@
 import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
 import {
     Dialog,
     DialogContent,
     DialogDescription,
     DialogHeader,
     DialogTitle,
-    // DialogTrigger,
 } from "@/components/ui/dialog"
-
-
-import { signIn } from "next-auth/react";
 
 export const LoginModal = ({ onClose, showLoginModal }: { onClose: () => void, showLoginModal: boolean }) => {
     return (
-        // <div className="absolute top-32 transform -translate-x-1/2 flex flex-col space-y-4 -translate-y-1/2 p-5 rounded-lg shadow-md z-50">
-        //     <h2></h2>
-        //     <p></p>
-        //     <button onClick={() => signIn("google")}>Login with Google</button>
-        //     <button onClick={onClose}>Close</button>
-        // </div>
-
         <Dialog open={showLoginModal} onOpenChange={(open) => {
             if (!open) {
                 onClose();
