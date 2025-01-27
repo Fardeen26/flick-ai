5bd9efe525c07885fcae7385c7b2bbfd62fb5131
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2025-01-27 15:59:14 +0530

    feat: added twitter intent

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -5,12 +5,14 @@ import { FaWandMagicSparkles } from "react-icons/fa6";
 import { ShinyButton } from '@/components/ui/shiny-button'
 import { FaHeart } from "react-icons/fa";
 import { ResultProps } from "@/types/ResultProps";
+import { RiTwitterXLine } from "react-icons/ri";
 import {
     Tooltip,
     TooltipContent,
     TooltipProvider,
     TooltipTrigger,
 } from "@/components/ui/tooltip"
+import Link from "next/link";
 
 
 export default function Result({ improvePrompt, isImprovingField, setImprovePrompt, handleRegenerate, copyToClipboard, saveInteraction }: ResultProps) {
@@ -48,6 +50,21 @@ export default function Result({ improvePrompt, isImprovingField, setImproveProm
                         </TooltipTrigger>
                         <TooltipContent>
                             <p>Save</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
+
+                <TooltipProvider>
+                    <Tooltip>
+                        <Link href={`https://twitter.com/intent/tweet?text=${result}`} target="_blank">
+                            <TooltipTrigger>
+                                <button className="bg-transparent rounded-lg before:bg-opacity-5 backdrop-blur-lg border border-gray-400/50 dark:border-white/20 dark:text-white p-2 max-sm:p-1 max-sm:px-1.5 hover:bg-white/10 transition-all duration-300">
+                                    <RiTwitterXLine className="w-3 h-3" />
+                                </button>
+                            </TooltipTrigger>
+                        </Link>
+                        <TooltipContent>
+                            <p>Share on X</p>
                         </TooltipContent>
                     </Tooltip>
                 </TooltipProvider>
